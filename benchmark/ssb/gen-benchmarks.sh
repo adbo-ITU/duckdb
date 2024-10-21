32fd3034fb3be0e5db50902bab9c4d2d0c94fd9d
Adrian Borup <adrian@borup.com>
2024-10-21 09:21:53 +0200

    Use ssbgen instead of data files to populate data

--- benchmark/ssb/gen-benchmarks.sh
+++ benchmark/ssb/gen-benchmarks.sh
@@ -8,16 +8,6 @@
 # │   ├── q1.2.sql
 # │   ├── q4.2.sql
 # │   └── ...
-# ├── data
-# │   ├── sf1
-# │   │   ├── customer.tbl
-# │   │   ├── date.tbl
-# │   │   ├── lineorder.tbl
-# │   │   ├── part.tbl
-# │   │   └── supplier.tbl
-# │   └── sf10
-# │       ├── customer.tbl
-# │       └── ...
 # └── ssb.benchmark.in
 #
 # This script will then generate all the benchmark files for the SSB benchmark, i.e.:
@@ -37,6 +27,7 @@
 #     └── load-ssb-sf10.sql
 
 threads=(1 4 8)
+sfs=(1 10 100)
 
 for thread in "${threads[@]}"; do
   echo "Generating queries for $thread threads..."
@@ -51,10 +42,7 @@ for thread in "${threads[@]}"; do
   done
 done
 
-for sf_dir in ./benchmark/ssb/data/*; do
-  sf_dir_name=$(basename $sf_dir)
-  scaling_factor="${sf_dir_name#"sf"}"
-
+for scaling_factor in "${sfs[@]}"; do
   for query_file in ./benchmark/ssb/queries/*.sql; do
     query_number=$(echo $query_file | awk -F'[/q.]' '{print $(NF-3) "." $(NF-2)}')
 
@@ -74,98 +62,4 @@ THREADS=$thread"
       echo "$bench_file" > "./benchmark/ssb/benchmarks/sf$scaling_factor/q$query_number-t$thread.benchmark"
     done
   done
-
-  echo "Generating loader file for scaling factor $scaling_factor..."
-
-  load_file="CREATE TABLE part
-(
-    p_partkey   UINTEGER,
-    p_name      VARCHAR,
-    p_mfgr      VARCHAR,
-    p_category  VARCHAR,
-    p_brand1    VARCHAR,
-    p_color     VARCHAR,
-    p_type      VARCHAR,
-    p_size      UTINYINT,
-    p_container VARCHAR,
-    PRIMARY KEY (p_partkey)
-);
-
-CREATE TABLE supplier
-(
-    s_suppkey UINTEGER,
-    s_name    VARCHAR,
-    s_address VARCHAR,
-    s_city    VARCHAR,
-    s_nation  VARCHAR,
-    s_region  VARCHAR,
-    s_phone   VARCHAR,
-    PRIMARY KEY (s_suppkey)
-);
-
-CREATE TABLE customer
-(
-    c_custkey    UINTEGER,
-    c_name       VARCHAR,
-    c_address    VARCHAR,
-    c_city       VARCHAR,
-    c_nation     VARCHAR,
-    c_region     VARCHAR,
-    c_phone      VARCHAR,
-    c_mktsegment VARCHAR,
-    PRIMARY KEY (c_custkey)
-);
-
-CREATE TABLE date
-(
-    d_datekey          DATE,
-    d_date             VARCHAR,
-    d_dayofweek        VARCHAR,
-    d_month            VARCHAR,
-    d_year             USMALLINT,
-    d_yearmonthnum     UINTEGER,
-    d_yearmonth        VARCHAR,
-    d_daynuminweek     UTINYINT,
-    d_daynuminmonth    UTINYINT,
-    d_daynuminyear     USMALLINT,
-    d_monthnuminyear   UTINYINT,
-    d_weeknuminyear    UTINYINT,
-    d_sellingseason    VARCHAR,
-    d_lastdayinweekfl  BOOLEAN,
-    d_lastdayinmonthfl BOOLEAN,
-    d_holidayfl        BOOLEAN,
-    d_weekdayfl        BOOLEAN,
-    PRIMARY KEY (d_datekey)
-);
-
-CREATE TABLE lineorder
-(
-    lo_orderkey      UINTEGER,
-    lo_linenumber    UTINYINT,
-    lo_custkey       UINTEGER,
-    lo_partkey       UINTEGER,
-    lo_suppkey       UINTEGER,
-    lo_orderdate     DATE,
-    lo_orderpriority VARCHAR,
-    lo_shippriority  VARCHAR,
-    lo_quantity      UTINYINT,
-    lo_extendedprice UINTEGER,
-    lo_ordtotalprice UINTEGER,
-    lo_discount      UTINYINT,
-    lo_revenue       UINTEGER,
-    lo_supplycost    UINTEGER,
-    lo_tax           UTINYINT,
-    lo_commitdate    DATE,
-    lo_shipmode      VARCHAR,
-    PRIMARY KEY (lo_orderkey, lo_linenumber)
-);
-
-COPY part FROM 'benchmark/ssb/data/sf$scaling_factor/part.tbl';
-COPY supplier FROM 'benchmark/ssb/data/sf$scaling_factor/supplier.tbl';
-COPY customer FROM 'benchmark/ssb/data/sf$scaling_factor/customer.tbl';
-COPY date FROM 'benchmark/ssb/data/sf$scaling_factor/date.tbl';
-COPY lineorder FROM 'benchmark/ssb/data/sf$scaling_factor/lineorder.tbl';"
-
-    mkdir -p "./benchmark/ssb/init"
-    echo "$load_file" > "./benchmark/ssb/init/load-ssb-sf$scaling_factor.sql"
 done
